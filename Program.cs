efc90c501b5cc705d165ec3f4bd6db3367f115f4
unknown <mohammadi.ehsan1994@gmail.com>
2019-07-30 11:05:51 +0430

    Add Vcpkg.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using VCPKG;
 
 namespace vcpkg_port_update_alert
 {
     class Program
     {
+        static void PrintHeaders()
+        {
+            Console.WriteLine("vcpkg port update alert");
+            Console.WriteLine("Version 1.0");
+            Console.WriteLine("By Ehsan Mohammadi");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to start checking ports...");
+        }
+
         static void Main(string[] args)
         {
-            
+            PrintHeaders();
+
+            Vcpkg vcpkg = new Vcpkg();
+            vcpkg.GetVcpkgRepo();
+            Console.ReadKey();
         }
     }
 }
